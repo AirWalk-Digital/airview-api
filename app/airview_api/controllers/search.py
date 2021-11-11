3ef4c4791c439b1c2b524401c9ad09060bbc7791
Adrian Fernandez <adrian@luengo.co>
2021-11-11 11:51:58 +0000

    Update search endpoint with correct param

--- app/airview_api/controllers/search.py
+++ app/airview_api/controllers/search.py
@@ -15,15 +15,15 @@ blp = Blueprint(
 search_backend = init_search_backend()
 
 
-@blp.route("/search")
+@blp.route("/<string:search_terms>")
 class Search(MethodView):
     @blp.response(200, SearchResultSchema(many=True))
     @blp.role(Roles.CONTENT_READER)
-    def get(self, args):
+    def get(self, search_terms):
         """
         Searches matching terms in a configured search backend.
         """
         results = []
         if search_backend:
-            results = search_backend.query(**args)
+            results = search_backend.query(search_terms)
         return results
