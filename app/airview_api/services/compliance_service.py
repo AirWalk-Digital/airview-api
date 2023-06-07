dbbbdd09d9248ac57228195799a546b41ebbcb06
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2023-06-07 14:59:55 +0100

    Tidy up compliance aggreagation and add comments

--- app/airview_api/services/compliance_service.py
+++ app/airview_api/services/compliance_service.py
@@ -1,5 +1,3 @@
-from odata_query.sqlalchemy import apply_odata_query
-from odata_query.sql import AstToSqlVisitor
 from airview_api.models import (
     Application,
     Resource,
@@ -11,24 +9,26 @@ from airview_api.database import db
 from sqlalchemy import func, case
 from sqlalchemy.sql.expression import cte
 
-from sqlalchemy.dialects import postgresql
 from odata_query.grammar import ODataParser, ODataLexer
+from odata_query.sql import AstToSqlVisitor
 
 
 def get_compliace_aggregate(filter: str, select: str):
-    filter = filter.lower()
+    # sql alchemy gets in a tizz about casing. for the purposes of this method, everything is lowercased then converted to snake case on return
     select = select.lower()
+
+    # Check that the selected columns are within the defined list, otherwise the query will break
     splits = select.replace(" ", "").split(",")
     allowed_columns = ["application_id", "resource_reference"]
-    mapping = {x.replace("_", ""): x for x in allowed_columns}
-    # all(any(x in y for y in allowed_columns) for x in splits)
+    # todo: add assertion
+    all(any(x in y for y in allowed_columns) for x in splits)
 
+    # Create an initial subquery query to flatten the data.
+    # Columns are lower cased to avoid issues with case sensitivity & relabled to avoid naming collisions
     orm_query = (
         db.select(
             Resource.reference.label("resourcereference"),
             Application.id.label("applicationid"),
-            # Application.name.label("application_name"),
-            # TechnicalControl.reference.label("technical_control_reference"),
             case(
                 [
                     (
@@ -45,31 +45,22 @@ def get_compliace_aggregate(filter: str, select: str):
         .join(Resource)
         .join(Application)
     )
-    # print(orm_query.compile(db.engine, compile_kwargs={"literal_binds": True}))
-    # print(q2.compile(db.engine, compile_kwargs={"literal_binds": True}))
-    # results = db.session.execute(orm_query).all()
-    # print(results)
 
-    # print(orm_query.compile(db.engine, compile_kwargs={"literal_binds": True}))
+    if filter:
+        # Use external odata filter code to pase the incoming odata query into a sql where clause
+        filter = filter.lower()
+        lexer = ODataLexer()
+        parser = ODataParser()
+        ast = parser.parse(lexer.tokenize(filter))
+        visitor = AstToSqlVisitor()
+        where_clause = visitor.visit(ast)
+        # apply the where to the subquery
+        orm_query = db.select(["*"]).select_from(orm_query).where(db.text(where_clause))
 
-    lexer = ODataLexer()
-    parser = ODataParser()
-    ast = parser.parse(lexer.tokenize(filter))
-    visitor = AstToSqlVisitor()
-    where_clause = visitor.visit(ast)
-    where_query = (
-        db.select(
-            ["*"]
-            # db.column("resource_reference"),
-            # db.column("application_id"),
-            # db.column("isCompliant"),
-        )
-        .select_from(orm_query)
-        .where(db.text(where_clause))
-    )
+    # The mapping is used to re-label the previously lower cased columns back to snake case, python style
+    mapping = {x.replace("_", ""): x for x in allowed_columns}
 
-    # query = apply_odata_query(orm_query, filter) if filter is not None else orm_query
-
+    # apply the final 'group by' aggregation based on what was requested in the select param
     aggreated_query = (
         db.select(
             [db.column(c).label(mapping[c]) for c in splits]
@@ -78,12 +69,9 @@ def get_compliace_aggregate(filter: str, select: str):
                 func.count(db.column("isCompliant")).label("total"),
             ]
         )
-        .select_from(where_query)
+        .select_from(orm_query)
         .group_by(db.text(select))
     )
-
-    # print(type(aggreated_query))
-    # print(aggreated_query.compile(db.engine, compile_kwargs={"literal_binds": True}))
 
     results = db.session.execute(aggreated_query).all()
 
